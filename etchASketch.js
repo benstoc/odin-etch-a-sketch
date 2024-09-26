7a8725597b6c74e40dd002737f3295feea5b2f08
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-09-26 10:40:57 -0700

    Add button to create a new grid
    Also refactored the code so a grid is generated when the page loads

--- etchASketch.js
+++ etchASketch.js
@@ -1,18 +1,32 @@
 const CONTAINER_SIZE_PX = 700;
 
 function getDimensions () {
-    return prompt("Enter the desired grid height:");
+    let height;
+    while (true) {
+        height = +prompt("Enter the desired grid height (max of 100):");
+        console.log(height, typeof height)
+        if (height === null) {
+            break;
+        } else if (typeof height === "number" && !isNaN(height)) {
+            break;
+        } else {
+            alert("Invalid input, please try again.");
+        }
+    }
+    if (height > 100) {
+        alert("Input was over max height. \nCreating 100x100 grid.");
+        return 100;
+    };
+    return height
 }
 
 function calcBoxSize (dimensions) {
     return Math.floor(CONTAINER_SIZE_PX / dimensions);
 }
 
-function createBoxGrid () {
-    const dimensions = getDimensions();
+function createBoxGrid (dimensions = 16) {
     const boxSize = calcBoxSize(dimensions) + "px";
     const numOfBoxes = dimensions * dimensions
-
     const gridContainer = document.querySelector(".container");
     gridContainer.style.paddingBottom = (dimensions - 1) + "px"; // temp
 
@@ -22,17 +36,24 @@ function createBoxGrid () {
         div.style.width = boxSize;
         div.style.height = boxSize;
 
+        div.addEventListener("mouseenter", () => {
+            div.classList.add("color-box");
+        });
 
-        gridContainer.appendChild(div)
+        gridContainer.appendChild(div);
     }
 }
 
 createBoxGrid()
 
+const gridContainer = document.querySelector(".container");
 const gridBoxes = document.querySelectorAll(".box");
-gridBoxes.forEach((box) => {
-    box.addEventListener("mouseenter", ()=> {
-        console.log("test")
-        box.classList.add("color-box");
-    });
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    const dimensions = getDimensions();
+    if (dimensions) {
+        gridContainer.innerHTML = "";
+        createBoxGrid(dimensions);
+    };
 });
