695e05dc854e83bac4f2eb329a288676048a681c
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-09-26 09:20:58 -0700

    Implement grid generation functionality

---
+++ etchASketch.js
@@ -0,0 +1,29 @@
+const CONTAINER_SIZE_PX = 700;
+
+function getDimensions () {
+    return prompt("Enter the desired grid height:");
+}
+
+function calcBoxSize (dimensions) {
+    return Math.floor(CONTAINER_SIZE_PX / dimensions);
+}
+
+function createBoxGrid () {
+    const dimensions = getDimensions();
+    const boxSize = calcBoxSize(dimensions) + "px";
+    const numOfBoxes = dimensions * dimensions
+
+    const gridContainer = document.querySelector(".container");
+
+    for (let i = 0; i < numOfBoxes; i++) {
+        const div = document.createElement("div")
+        div.classList.add("box");
+        div.style.width = boxSize;
+        div.style.height = boxSize;
+
+
+        gridContainer.appendChild(div)
+    }
+}
+
+createBoxGrid()
